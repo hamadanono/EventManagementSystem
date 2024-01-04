11c217e60eeb4e97573a873900db498f4848753b
hamadanono <154978569+hamadanono@users.noreply.github.com>
2024-01-04 19:34:52 +0800

    Update navigation_student.php

--- navigation_student.php
+++ navigation_student.php
@@ -1,7 +1,7 @@
 <nav class="topnav">
     <a href="portfolio.php">NAV 1</a>
     <a href="kpi.php">NAV 2</a>
-    <a href="event.php">LIST OF EVENTS</a>
+    <a href="eventboard.php">EVENT</a>
     <a href="mycompetition.php">NAV 4</a>
     <a href="mycertification.php">NAV 5</a>
     <a href="mychallenge.php">NAV 6</a>
