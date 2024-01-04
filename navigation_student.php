8268311b3f9625beb9996d2aa8c1973c69397808
zawawieamin99 <155613069+zawawieamin99@users.noreply.github.com>
2024-01-04 18:53:41 +0800

    Add files via upload

---
+++ navigation_student.php
@@ -0,0 +1,9 @@
+<nav class="topnav">
+    <a href="portfolio.php">NAV 1</a>
+    <a href="kpi.php">NAV 2</a>
+    <a href="event.php">LIST OF EVENTS</a>
+    <a href="mycompetition.php">NAV 4</a>
+    <a href="mycertification.php">NAV 5</a>
+    <a href="mychallenge.php">NAV 6</a>
+	<a href="logout.php">Log Out</a>
+</nav>
