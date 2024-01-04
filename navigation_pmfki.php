11b0c4cd18e29068c08f677b762c60862854f3f6
zawawieamin99 <155613069+zawawieamin99@users.noreply.github.com>
2024-01-05 00:21:10 +0800

    Update navigation_pmfki.php

--- navigation_pmfki.php
+++ navigation_pmfki.php
@@ -1,7 +1,7 @@
 <nav class="topnav">
     <a href=".php">NAV 1</a>
     <a href="proposal_pmfki.php">Proposal</a>
-    <a href=".php">NAV 3</a>
+    <a href="event.php">Event</a>
     <a href=".php">NAV 4</a>
     <a href=".php">NAV 5</a>
     <a href=".php">NAV 6</a>
